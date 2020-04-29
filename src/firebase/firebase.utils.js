791f67c2ef0d5f49908a36332774fa4d72669647
VincentCSS <vincenthtml@gmail.com>
2020-04-28 20:27:02 -0600

    finished redux setup and fixed firebase permissions

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAvFgwvZ2CXZKwVmlX9Q3Qf-B7PIWhu4hU",
-    authDomain: "ecom-fire.firebaseapp.com",
-    databaseURL: "https://ecom-fire.firebaseio.com",
-    projectId: "ecom-fire",
-    storageBucket: "ecom-fire.appspot.com",
-    messagingSenderId: "883650701353",
-    appId: "1:883650701353:web:8f8ccd5c055a151b18d438",
-    measurementId: "G-0WGFETL25N"
+  apiKey: "AIzaSyAvFgwvZ2CXZKwVmlX9Q3Qf-B7PIWhu4hU",
+  authDomain: "ecom-fire.firebaseapp.com",
+  databaseURL: "https://ecom-fire.firebaseio.com",
+  projectId: "ecom-fire",
+  storageBucket: "ecom-fire.appspot.com",
+  messagingSenderId: "883650701353",
+  appId: "1:883650701353:web:8f8ccd5c055a151b18d438",
+  measurementId: "G-0WGFETL25N"
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
