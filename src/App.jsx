105ba31fdce5b1a52e333478be8409defd4e3df0
VincentCSS <vincenthtml@gmail.com>
2020-05-06 17:27:59 -0600

    added dummy data to firestore and fetched accordingly. added loading spinner to account for async nature

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -17,7 +15,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
 
-import './App.css';
+import './App.scss';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
